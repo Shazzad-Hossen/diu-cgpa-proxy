81ee7a9410534df63dd01631344e9e17c836cdf1
Shazzad-Hossen <tipsfire.com@gmail.com>
2024-06-09 01:46:38 +0600

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { default: axios } = require('axios');
 const app = express();
-const port=4000;
+const port=process.env.PORT ||4000;
 app.use(cors());
 app.use(express.json());
-
 
 app.get('/',async(req,res)=>{
     res.status(200).send('Yoyo')
